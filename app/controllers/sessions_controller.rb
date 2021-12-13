b5b4fb9d0a4b34053b65556cee2b94a9df82f6f9
makicamel <unright@gmail.com>
2021-12-14 08:03:34 +0900

    Add SessionsController

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user&.authenticate(params[:session][:password])
+      sign_in(user)
+      # redirect_to foo_path
+    else
+      flash[:error] = 'Wrong email or password'
+      render :new
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to new_path
+  end
+
+  private
+
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
+
+  def sign_out
+    session.delete
+    reset_session
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'sessions#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end
