0313f41efcf4fd2587994b57aaeedfe7b8e0254c
makicamel <unright@gmail.com>
2021-12-15 05:04:57 +0900

    rails g item

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_220919) do
+ActiveRecord::Schema.define(version: 2021_12_14_200318) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
