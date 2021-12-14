e07d92c1f477d470aae5ae01453a6d03c35316f6
makicamel <unright@gmail.com>
2021-12-15 05:56:38 +0900

    Authenticate user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :authentecate_user!
+
   private
+
+  def authentecate_user!
+    if session[:user_id].nil?
+      redirect_to login_path
+    end
+  end
 
   helper_method :current_user
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :authentecate_user!
+
   def new; end
 
   def create
